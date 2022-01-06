7118ecd49aa15fcbf48949a0949ff2884bcf070f
Ritik Khatana <54929382+ritikkhatana79020@users.noreply.github.com>
2022-01-06 18:30:21 +0530

    Time: 4 ms (86.75%), Space: 38.8 MB (89.06%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reverseWords(String s) {
+        String arr[] = s.split(" ");
+        
+	    StringBuilder sb = new StringBuilder("");
+	    
+        for (int i = arr.length - 1; i >= 0; i--) {
+		
+            if ("".equals(arr[i])) {
+                continue;
+            }
+            
+		    sb.append(arr[i]).append(" ");
+	    }
+ 	    String result = sb.toString();
+        return result.trim();
+        
+    }
+}
