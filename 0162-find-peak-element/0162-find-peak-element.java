82100c833ab972768954d0b381881e1aec32b3f7
Ritik Khatana <54929382+ritikkhatana79020@users.noreply.github.com>
2023-09-03 17:35:34 +0530

    Time: 0 ms (100%), Space: 40.9 MB (87.02%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int peak=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[peak]){
+                peak=i;
+            }else{
+                break;
+            }
+        }
+        return peak;
+    }
+}
