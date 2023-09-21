baa938adca4f3a6bc08c0ddf914d5ab6dd3b42ee
Ritik Khatana <54929382+ritikkhatana79020@users.noreply.github.com>
2023-09-21 11:56:09 +0530

    Time: 16 ms (24.53%), Space: 43.4 MB (55.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,13 +3,8 @@ class Solution {
     
         HashMap<Character, Integer> map = new HashMap<>();
 
-
-        for(int i=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
-            }else{
-                map.put(s.charAt(i),1);
-            }
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
 
         for(int i=0;i<t.length();i++){
