f5fd06c442609b60112e62be7e1b2b1953c3b24c
Ritik Khatana <54929382+ritikkhatana79020@users.noreply.github.com>
2023-10-26 23:28:44 +0530

    Time: 0 ms (100%), Space: 43.8 MB (29.57%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -18,11 +18,11 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow==fast){
-                slow=head;
+            if(slow == fast){
+                slow = head;
                 while(slow!=fast){
-                    slow=slow.next;
-                    fast=fast.next;
+                    slow =slow.next;
+                    fast =fast.next;
                 }
                 return slow;
             }
